7b68bc693b0c696b16646c272e263dcbf9f1a2ba
Nilesh Raghunath Patil <nil.patil@tcs.com>
2020-08-02 12:08:30 +0530

    Added HTTP request, updating states, env variables and functional components

--- src/App.js
+++ src/App.js
@@ -2,14 +2,30 @@ import React, { Component } from 'react';
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import Axios from 'axios';
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: false,
+  };
+
+  async componentDidMount() {
+    this.setState({ loading: true });
+
+    const res = await Axios.get(
+      `https://api.github.com/users?client_id=${process.env.REACT_APP_CLIENT_ID}&client_secret=${process.env.REACT_APP_CLIENT_SECRET}`
+    );
+
+    this.setState({ users: res.data, loading: false });
+  }
+
   render() {
     return (
       <div className='App'>
         <Navbar />
         <div className='container'>
-          <Users />
+          <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
     );
